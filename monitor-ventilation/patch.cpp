59d38746fe07432730209a21b8f4ccac28d768ec
Matt <mw283@cam.ac.uk>
2021-07-03 12:06:13 +0100

    deleted:    calc-resp-rate/patch.xodp 	deleted:    detect-inspiration-expiration/patch.xodp 	modified:   monitor-ventilation/patch.cpp 	modified:   monitor-ventilation/patch.xodp 	deleted:    moving-average-3/patch.xodp 	deleted:    record-max-flows-copy/patch.xodp 	deleted:    record-max-flows/patch.xodp 	deleted:    reset/patch.cpp 	deleted:    reset/patch.xodp 	deleted:    respiratory-monitor/patch.xodp

--- monitor-ventilation/patch.cpp
+++ monitor-ventilation/patch.cpp
@@ -1,44 +1,84 @@
-
 node {
+    // Volume (cm3)
+    Number vol=0;
+    // Flow has positive value: true for inspiration/insufflation, false for expiration/exsufflation.
+    bool posFlow = true;
+    // Previous value of posFlow.
+    bool posFlowPrev = true;
+    // Previous flow measurement time (ms).
+    TimeMs mt_prev;
+    // Time of latest inspiration (ms).
+    TimeMs ins_t = 0;
+    // Time of previous inspiration (ms).
+    TimeMs ins_t_prev = 0;
+    // Flow rate (standard litres per minute).
+    // Flow has positive value for inspiration/insufflation, negative for expiration/exsufflation.
+    Number flow;
 
-    Number vol; // Volume (cm3)
-    bool posFlow = false; // Flow has positive value: true for inspiration, false for expiration
-    bool posFlowPrev = false; // Previous value of posFlow
-    TimeMs mt_prev = transactionTime();
-    TimeMs ins_prev = mt_prev;
-    
     void evaluate(Context ctx) {
-        
+
         // Measure flow rate only if there is an input pulse.
         if (!isInputDirty<input_UPD>(ctx))
             return;
 
+        // Check for reset
+        bool rst = isInputDirty<input_RST>(ctx);
+
         TimeMs mt = transactionTime();
         auto sensor = getValue<input_DEV>(ctx);
-        Number flow = sensor->readFlow();
 
-        // check direction of flow
-        posFlow = 0 < round(flow); // round to prevent small fluctuations from changing value of posFlow
+        // soft reset
+        if (rst) {
+            sensor->sendCommand(SOFT_RESET);
+            delay(100);
+            sensor->begin();
+        }
 
-        if (isSettingUp()){
-            posFlowPrev=posFlow;
-            vol=0;
+        // Setting up. If we start measuring during an inspiration/insufflation or expiration/exsufflation,
+        // the first measurement will be inaccurate. The following code makes the program wait for an
+        // expiration/exsufflation before starting measurements.
+        if (isSettingUp() || rst){
+            uint8_t flow_dir_changes = 0;
+            while (flow_dir_changes<2) {
+                // measure flow
+                flow = sensor->readFlow();
+                // check direction of flow
+                posFlow = 0 < round(flow);
+                if (posFlowPrev!=posFlow) {
+                    flow_dir_changes += 1;
+                    if(posFlowPrev) { 
+                        ins_t_prev=mt;
+                    }
+                    posFlowPrev=posFlow;
+                }
+                delay(20);
+            }
             return;
         }
         
-        if (posFlowPrev!=posFlow) { // change of flow direction
+        // measure flow
+        flow = sensor->readFlow();
+        // check direction of flow
+        posFlow = 0 < round(flow); // round to prevent small fluctuations around zero from changing value of posFlow
+
+        // check for change of flow direction
+        if (posFlowPrev!=posFlow) {
             // if direction of flow has changed
             if(posFlowPrev) {
                 emitValue<output_iVOL>(ctx, vol);
                 emitValue<output_INS>(ctx, 1);
-                emitValue<output_RR>(ctx, 60/(mt-ins_prev));
-                ins_prev=mt;
+                ins_t=mt;
+                //TimeMs resp_delta = ins_t-ins_t_prev;
+                Number resp_rate = 60/((ins_t-ins_t_prev)/1000.0);
+                emitValue<output_RR>(ctx, resp_rate);
+                ins_t_prev=ins_t;
             } else {
                 emitValue<output_eVOL>(ctx, -vol);
                 emitValue<output_EXP>(ctx, 1);
             }
             posFlowPrev=posFlow;
-            vol = 0; // reset the volume
+            // reset volume
+            vol = 0;
         }
 
         TimeMs mt_delta = mt - mt_prev;
