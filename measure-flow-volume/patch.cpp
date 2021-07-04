ce9dfa7db72596e14969915704d907031d9acf8a
Matt <mw283@cam.ac.uk>
2021-07-04 11:00:38 +0100

    deleted:    example-test-respiratory-monitor/patch.xodp 	new file:   measure-flow-volume/patch.cpp 	new file:   measure-flow-volume/patch.xodp 	modified:   monitor-insufflation/patch.xodp

---
+++ measure-flow-volume/patch.cpp
@@ -0,0 +1,59 @@
+node {
+    // Volume (cm3)
+    Number iVol=0;
+    Number eVol=0;
+    TimeMs mt_prev;
+    // Time of latest inspiration (ms).
+    TimeMs ins_t = 0;
+    // Time of previous inspiration (ms).
+    TimeMs ins_t_prev = 0;
+    // Flow rate (standard litres per minute).
+    // Flow has positive value for inspiration/insufflation, negative for expiration/exsufflation.
+    Number flow;
+
+    void evaluate(Context ctx) {
+
+        // soft reset
+        if (isInputDirty<input_RST>(ctx)) {
+            sensor->sendCommand(SOFT_RESET);
+            delay(100);
+            sensor->begin();
+        }
+
+        // Measure flow rate only if there is an input pulse.
+        if (!isInputDirty<input_UPD>(ctx))
+            return;
+
+        TimeMs mt = transactionTime();
+
+        if (isSettingUp()) {
+            return
+        }
+
+        auto sensor = getValue<input_DEV>(ctx);
+
+
+        // Measure flow rate, volume and respiratory rate.
+        flow = sensor->readFlow();
+
+        TimeMs mt_delta = mt - mt_prev;
+        mt_prev = mt;
+
+        if (isInputDirty<input_RVol>(ctx)) {
+            iVol=0;
+            eVol=0;
+        } else {
+            if (getValue<input_VolEn){
+                if (flow>0) {
+                    vol += flow/60*mt_delta;
+                } else {
+                    eVol +=
+                }
+            }
+        }
+
+        emitValue<output_FR>(ctx, flow);
+        emitValue<output_iVOL>(ctx, vol);
+        emitValue<output_DONE>(ctx, 1);
+    }
+}
